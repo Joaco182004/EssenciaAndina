b817de21b9d95915fb92433db81c1a86687dc90c
Joaco182004 <87869138+Joaco182004@users.noreply.github.com>
2023-11-03 20:39:05 -0500

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,7 @@
 const btn_menu=document.getElementById('btn-menu');
 const menu=document.getElementById('content-slider')
 btn_menu.addEventListener('click',()=>{
+    console.log('Menú');
     if(menu.style.height=='0px'){
         menu.style.height='400px';
     }
